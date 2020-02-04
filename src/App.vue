534dca5660b93a45c4c535f2321e2ee40717d750
fiotto <fiotto42@gmail.com>
2020-02-04 23:46:39 +0900

    add sync

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <input-props></input-props>
   <input-emit></input-emit>
+  <input-sync></input-sync>
 </template>
 
 <script>
 import InputProps from './InputProps/Index.vue'
 import InputEmit from './InputEmit/Index.vue'
+import InputSync from './InputSync/Index.vue'
 
 export default {
   components: {
     InputProps,
-    InputEmit
+    InputEmit,
+    InputSync
   }
 }
 </script>
---
+++ src/InputSync/MyInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text" :value="value"/>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  setup(props) {
+    const onInput = ($event) => {
+      context.emit('update:value', $event.target.value)
+    }
+    return {
+      onInput
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
